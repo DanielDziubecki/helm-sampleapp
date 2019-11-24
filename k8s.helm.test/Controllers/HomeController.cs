668b61dc53f50af82d5856b1c9a7a19a8fa8ebad
DanielDziubecki <daniel.dziubecki@wp.pl>
2019-11-24 18:42:40 +0100

    init

---
+++ k8s.helm.test/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using k8s.helm.test.Models;
+using Microsoft.Extensions.Configuration;
+
+namespace k8s.helm.test.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IConfiguration _configuration;
+
+        public HomeController(ILogger<HomeController> logger, IConfiguration configuration)
+        {
+            _logger = logger;
+            _configuration = configuration;
+        }
+
+        public IActionResult Index()
+        {
+            ViewBag.dbName = _configuration["mongo:DatabaseName"];
+            ViewBag.dbConString = _configuration["mongo:ConnectionString"];
+            ViewBag.localConfig = _configuration["localConfig"];
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
